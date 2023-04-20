3d6264b5f53a62883459121d551d3723cac1c79b
Keith Hetrick <keithjhetrick@gmail.com>
2023-04-20 18:24:16 -0500

    updated commenting

--- Section5/Comments/main.cpp
+++ Section5/Comments/main.cpp
@@ -1,9 +1,9 @@
 /*****************************************************************
- * Name: Favorite Number
+ * Name: Comments
  * Author: Keith Hetrick
  * Course: Beginning C++ Programming - Udemy
- * Program: Favorite Number
- * Description: This program allows the user to enter their favorite number between 1 and 100
+ * Program: Comments
+ * Description: This program demonstrates the use of comments in C++
  * Input: User input
  * Output: Console output
  * Last Modified: 04/20/2023
@@ -11,12 +11,23 @@
 
 #include <iostream>
 
+/*
+  Example
+  of
+  multi-line
+  comment
+*/
+
+// Example of single line comment
+
 int main()
 {
   int favorite_number;
   std::cout << "Ingrese su número favorito entre 1 y 100: ";
   std::cin >> favorite_number;
-  std::cout << "No, en serio, " << favorite_number << " es mi número favorito tambien!" << std::endl;
+  std::cout << "¡Increíble, ese es mi número favorito también!" << std::endl;
+  std::cout << "No, en serio, " << favorite_number << " es mi número favorito." << std::endl;
+  std::cout << "El programa se ejecutó en " << __TIME__ << std::endl;
 
   return 0;
 }
